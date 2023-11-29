714febd57e8003348f6778a8c50ac33b84553f0d
sinai <sinailei@gmail.com>
2023-11-30 00:08:14 +0200

    finished part A - doesn't work tho

--- app/src/main/java/com/example/advancedcanvas/MainActivity.java
+++ app/src/main/java/com/example/advancedcanvas/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.advancedcanvas;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    WritingView writingView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        writingView = findViewById(R.id.myview);
+
+        //writingView = new WritingView(this);
     }
 }
--- app/src/main/java/com/example/advancedcanvas/WritingView.java
+++ app/src/main/java/com/example/advancedcanvas/WritingView.java
@@ -5,16 +5,21 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.LayoutDirection;
 import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
 
 public class WritingView extends View
 {
     Paint paint;
     int x1, y1, x2, y2;
     Mypoint lastpoint;
+    ArrayList<Line> lines = new ArrayList<>();
+
 
     public WritingView(Context context) {
         super(context);
@@ -39,27 +44,31 @@ public class WritingView extends View
         super.onDraw(canvas);
 
         // All things will be drawn here
+        for (Line l :
+             lines) {
+            canvas.drawLine(l.start.x, l.start.y, l.end.x, l.end.y, paint);
+        }
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-
-        int x = (int) event.getX();
-        int y = (int) event.getY();
-
         if (event.getAction() == MotionEvent.ACTION_DOWN)
-        {}
+        {
+            x1 = (int) event.getX();
+            y1 = (int) event.getY();
+            lastpoint = new Mypoint(x1, y1);
+        }
         else if (event.getAction() == MotionEvent.ACTION_MOVE)
         {
-
+            x2 = (int) event.getX();
+            y2 = (int) event.getY();
+            Mypoint newpoint = new Mypoint(x2, y2);
+            lines.add(new Line(lastpoint, newpoint));
+            lastpoint = newpoint;
+            invalidate();
         }
         else
-        {
-
-        }
-
-
-
+        {}
         return super.onTouchEvent(event);
     }
 }
