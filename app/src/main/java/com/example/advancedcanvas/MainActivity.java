decda63f8e786f22116657e394d2ed9944ac208e
noa1107 <noadachbash@gmail.com>
2023-12-19 09:20:13 +0200

    added more code

--- app/src/main/java/com/example/advancedcanvas/MainActivity.java
+++ app/src/main/java/com/example/advancedcanvas/MainActivity.java
@@ -3,16 +3,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     WritingView writingView;
-    //Drawing1 drawing;
+    Drawing1 drawing;
 
 
-//
-    //ActivityMainBinding binding;
-    //
+
+    ActivityMainBinding binding;
 
     int mDafaultColor = Color.GREEN;
     float defStroke = 20f;
@@ -20,14 +20,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-        //binding = ActivityMainBinding.inflate(getLayoutInflater());
-        //
-        //setContentView(binding.getRoot());
-        //writingView = findViewById(R.id.myview);
-        //
+        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        writingView = findViewById(R.id.myview);
         writingView = new WritingView(this);
+        drawing = findViewById(R.id.myview1)
+        drawing = new Drawing1(this);
 
+        binding.undo.setOnClickListener(new View.OnClickListener())
+        {
+            @override
+        }
 
 
     }
